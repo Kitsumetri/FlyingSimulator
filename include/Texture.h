a0fb680ea910b5195239ad6b6c5f5b39c1d14480
Kitsu <krasilnikovkolya4654@gmail.com>
2023-05-26 05:11:10 +0300

    [#6] - Texture refactor

--- include/Texture.h
+++ include/Texture.h
@@ -1,7 +1,7 @@
 #ifndef TEXTURE_CLASS_H
 #define TEXTURE_CLASS_H
 
-#include<glad/glad.h>
+#include <glad/glad.h>
 #include "stb_image.h"
 
 #include "Shader.h"
@@ -9,19 +9,17 @@
 class Texture
 {
 public:
-    GLuint ID;
+    GLuint ID{};
     const char* type;
     GLuint unit;
 
     Texture(const char* image, const char* texType, GLuint slot);
 
     // Assigns a texture unit to a texture
-    void texUnit(Shader& shader, const char* uniform, GLuint unit);
-    // Binds a texture
-    void Bind();
-    // Unbinds a texture
-    void Unbind();
-    // Deletes a texture
+    static void texUnit(Shader& shader, const char* uniform, GLuint unit);
+
+    void Bind() const;
+    [[maybe_unused]] static void Unbind();
     void Delete();
 };
 #endif
