6ae0d84f40b853e3be21157ff12c46149e8ee1bf
Kitsu <krasilnikovkolya4654@gmail.com>
2023-05-26 04:59:24 +0300

    [#6] - Shader refactor

--- include/Shader.h
+++ include/Shader.h
@@ -1,12 +1,11 @@
 #ifndef SHADER_CLASS_H
 #define SHADER_CLASS_H
 
-#include<glad/glad.h>
-#include<string>
-#include<fstream>
-#include<sstream>
-#include<iostream>
-#include<cerrno>
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 std::string get_file_contents(const char* filename);
 
@@ -15,18 +14,12 @@ class Shader
 public:
     // Reference ID of the Shader Program
     GLuint ID;
-    // Constructor that build the Shader Program from 2 different shaders
     Shader(const char* vertexFile, const char* fragmentFile);
     Shader(const char* vertexFile, const char* fragmentFile, const char* geometryFile);
 
-    // Activates the Shader Program
-    void Activate();
-    // Deletes the Shader Program
-    void Delete();
+    void Activate() const;
+    void Delete() const;
 private:
-    // Checks if the different Shaders have compiled properly
-    void compileErrors(unsigned int shader, const char* type);
+    static void compileErrors(unsigned int shader, const char* type);
 };
-
-
 #endif
